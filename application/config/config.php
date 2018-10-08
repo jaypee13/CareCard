5e423bfea401637a33c8e88c762ecd90f7bb20e1
jaypee13 <jaypee13@users.noreply.github.com>
2018-10-09 00:08:44 +0800

    Change

--- application/config/config.php
+++ application/config/config.php
@@ -23,11 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://www.silanguenocarecard.com';
+// $config['base_url'] = 'https://www.silanguenocarecard.com';
 // $config['base_url'] = 'http://localhost:8080/CareCard';
 //$config['base_url'] = 'http://localhost:8666/CareCard';
 // $config['base_url'] = 'http://localhost:8567/AzureCard';
-// $config['base_url'] = 'http://localhost:8567/CareCard';
+$config['base_url'] = 'http://localhost:8567/CareCard';
 
 /*
 |--------------------------------------------------------------------------
